5ecc1951302d36ba564953881fdb34fa4dc69190
gskielian <greg@techlabeducation.com>
2013-05-13 22:15:21 -0600

    Update Player.js

--- Player.js
+++ Player.js
@@ -3,6 +3,20 @@
 var ak47 : boolean = false ; 
 var bullets : int = 0 ;
 var ak47Object : GameObject;
+var bulletPrefab : GameObject;
+var playerCamera : GameObject;
+
+function Update () {
+ if( Input.GetMouseButton(0) )
+ {
+    if(ak47)
+    {
+      var  bullet : GameObject = GameObject.Instatiate(bulletPrefab, );
+    }
+ 
+ }
+  
+}
 
 
 function PickUp (var itemName : String ) {
