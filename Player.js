ae5d3ce48b7f344da304067b5b5bfb1dc87d7d5f
gskielian <greg@techlabeducation.com>
2013-05-13 21:49:59 -0600

    Create Player.js

---
+++ Player.js
@@ -0,0 +1,15 @@
+#pragma strict
+
+var ak47 : boolean = false ; 
+var bullets : int = 0 ;
+
+
+function PickUp (var itemName : String ) {
+
+  if(itemName == "ak47")  
+  {
+    ak47 = true;  
+  }
+
+
+}
