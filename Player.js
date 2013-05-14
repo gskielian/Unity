9683def6db57edec2711e53968c77a787a7a456d
gskielian <greg@techlabeducation.com>
2013-05-13 21:54:35 -0600

    Update Player.js

--- Player.js
+++ Player.js
@@ -2,6 +2,7 @@
 
 var ak47 : boolean = false ; 
 var bullets : int = 0 ;
+var ak47Object : GameObject;
 
 
 function PickUp (var itemName : String ) {
@@ -9,6 +10,7 @@ function PickUp (var itemName : String ) {
   if(itemName == "ak47")  
   {
     ak47 = true;  
+    ak47Object.SetActive(true);
   }
 
 
